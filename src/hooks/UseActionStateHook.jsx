022f55fdd24cce19fbe727fc36d20f66702c9ced
Kishor6941 <phawadeks9049@gmail.com>
2025-04-09 12:51:30 +0530

    useActionState

--- src/hooks/UseActionStateHook.jsx
+++ src/hooks/UseActionStateHook.jsx
@@ -1,9 +1,62 @@
-import React from 'react'
+
+import React, { useActionState } from "react";
 
 const UseActionStateHook = () => {
-  return (
-    <div>useActionStateHook</div>
-  )
-}
+  let handleSubmit = async (prevData, formData) => {
+    let email = formData.get("email");
+    let password = formData.get("password");
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    console.log(email, password);
+    if (email && password) {
+      return { message: "Data submitted successfully", email, password };
+    } else {
+      return { error: "Please fill all the fields" };
+    }
+  };
 
-export default UseActionStateHook
+  const [data, action, pending] = useActionState(handleSubmit, {
+    email: "a@gmail.com",
+    password: "fdf",
+  });
+  console.log(data);
+  return (
+    <div className="col-md-6 offset-3">
+      <h1>useActionState Hook</h1>
+      <form action={action}>
+        <div className="form-group">
+          <label htmlFor="">Email</label>
+          <input
+            defaultValue={data?.email}
+            type="text"
+            placeholder="email"
+            className="form-control"
+            name="email"
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="">Password</label>
+          <input
+            defaultValue={data?.password}
+            type="password"
+            placeholder="password"
+            className="form-control"
+            name="password"
+          />
+        </div>
+        <div className="mt-3">
+          <button className="btn btn-outline-success" disabled={pending}>
+            Submit
+          </button>
+          {data?.error && <p className="text-danger">{data.error}</p>}
+          {data?.message && <p className="text-success">{data.message}</p>}
+          {data?.email && <p className="text-success">Email : {data.email}</p>}
+          {data?.password && (
+            <p className="text-success">Password : {data.password}</p>
+          )}
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default UseActionStateHook;
